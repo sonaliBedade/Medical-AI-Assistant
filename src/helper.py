8909e78e8a9feb343ee3beaacda8d7ed0a021030
sonaliB15 <sonalibedade15@gmail.com>
2025-09-27 01:02:58 -0700

    Evaluation

--- src/helper.py
+++ src/helper.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from typing import List
 from langchain.schema import Document
